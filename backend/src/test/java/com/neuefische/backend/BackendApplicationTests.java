88b4ed046341f5e8bfd2afbbaf166efe8f5ebb9e
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-07-24 11:18:38 +0200

    Initial commit

---
+++ backend/src/test/java/com/neuefische/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.neuefische.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
