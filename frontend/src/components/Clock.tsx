0b08398141d2f25e3245712588bc77d65c6e06cb
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-07-31 12:01:29 +0200

    improved my getDay method in frontend

--- frontend/src/components/Clock.tsx
+++ frontend/src/components/Clock.tsx
@@ -38,26 +38,25 @@ function Clock() {
         switch (dayInNumber) {
             case 0:
                 return "Sunday"
-                break;
+
             case 1:
                 return "Monday"
-                break;
+
             case 2:
                 return "Tuesday"
-                break;
+
             case 3:
                 return "Wednesday"
-                break;
+
             case 4:
                 return "Thursday"
-                break;
+
             case 5:
                 return "Friday"
             case 6:
                 return "Saturday"
-                break;
             default:
-                break;
+                return
         }
     }
 
