0d8c5a3f424e5c9393d1e9d74769ff800f98a67c
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-08-01 12:41:53 +0200

    completed delete and edit functions

--- backend/src/test/java/com/neuefische/backend/TodoIntegrationTest.java
+++ backend/src/test/java/com/neuefische/backend/TodoIntegrationTest.java
@@ -2,6 +2,8 @@ package com.neuefische.backend;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.neuefische.backend.models.Status;
+import com.neuefische.backend.models.Todo;
 import com.neuefische.backend.models.TodoWithOutId;
 import com.neuefische.backend.services.TodoService;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -49,5 +52,29 @@ class TodoIntegrationTest {
     void testUpcomingTodos() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/todos/upcoming"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void test_deleteTodo() throws Exception {
+        String nowTime = "2023-07-27 12:34:56.123456";
+        Todo newTodo = new Todo("1", "go bike riding", "2023-08-26T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime);
+        todoRepository.save(newTodo);
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/todos/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testEditTodo() throws Exception {
+        String nowTime = "2023-07-27 12:34:56.123456";
+        Todo newTodo = new Todo("1", "go bike riding", "2023-08-26T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime);
+        todoRepository.save(newTodo);
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/todos/1")
+                                .content(objectMapper.writeValueAsBytes(new Todo("1", "go to church", "2023-09-26T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime)))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {"id": "1", "plan":"go to church", "startTime": "2023-09-26T15:55:33.000+02:00", "status": "open", "createdAt":"2023-07-27 12:34:56.123456"}
+                        """));
     }
 }
--- backend/src/test/java/com/neuefische/backend/TodoServiceTest.java
+++ backend/src/test/java/com/neuefische/backend/TodoServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -72,17 +73,37 @@ class TodoServiceTest {
     @Test
     void getUpcomingTodos() {
         String nowTime = "2023-07-27 12:34:56.123456";
-        List<Todo> todos = Arrays.asList(
+        List<Todo> todos = List.of(
                 new Todo("1", "go hiking", "2023-08-23T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime)
         );
 
         when(dateFormaterService.calcTimeDiffInDays("2023-07-30T15:55:33.000+02:00")).thenReturn((long) 25);
 
         when(todoRepository.findAll()).thenReturn(todos);
-        List<Todo> actual = Arrays.asList(
+        List<Todo> actual = List.of(
                 new Todo("1", "go hiking", "2023-08-23T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime)
         );
         List<Todo> expected = todoService.getUpcomingTodos();
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDeleteTodo() {
+        todoService.deleteTodo("1");
+        verify(todoRepository).deleteById("1");
+    }
+
+    @Test
+    void testEditTodo() {
+        String nowTime = "2023-07-27 12:34:56.123456";
+        when(todoRepository.findById("1")).thenReturn(Optional.of(new Todo("1", "go bike riding", "2023-08-26T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime)));
+        TodoWithOutId todoWithOutId = new TodoWithOutId(
+                "go out to see friends",
+                "2023-08-23T15:55:33.000+02:00"
+        );
+
+        Todo actual = todoService.editTodo(todoWithOutId, "1");
+        Todo expected = new Todo("1", "go bike riding", "2023-08-26T15:55:33.000+02:00", Status.OPEN.getStatus(), nowTime);
+        Assertions.assertNotEquals(expected, actual);
     }
 }
